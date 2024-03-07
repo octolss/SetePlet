edcf89ab5c3b21e3aeed54c0c139c127bb8cfb3e
octolss <147706639+octolss@users.noreply.github.com>
2024-03-07 12:23:41 +0000

    auto commit

--- client.cpp
+++ client.cpp
@@ -4,41 +4,48 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+
+int PORT = 8888;
 using namespace std;
 
-int PORT = 8888
-
-int main(int argc, char **argv){
-    if(argc > 1){
-        PORT = atoi(argv[1])
+int main(int argc, char *argv[]) {
+    
+    if (argc > 1) {
+        PORT = atoi(argv[1]);
     }
+
     int sock, new_socket;
     long valread;
     struct sockaddr_in serv_address;
-    int addrlen = sizeof(sockaddr_in);
-    char *hello = "Hello from server";
+    int addrlen = sizeof(serv_address);
+    char *hello;
+    cout << "Enter your message: ";
+    cin >> hello;
+
     char buffer[1024] = {0};
 
-    if((sock = socket(AF_INET, SOCK_STREAM, 0)) == 0){
-        perror("Socket failed");
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("Socket Failed");
         exit(EXIT_FAILURE);
     }
 
     serv_address.sin_family = AF_INET;
-    //serv_address.sin_addr.s_addr = INADDR_ANY
     serv_address.sin_port = htons(PORT);
 
-    if( inet_pton(AF_INET,"127.0.0.1",&serv_address.sin_addr )<=0){
-        cout << "invalid adress" << endl;
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_address.sin_addr) <= 0) {
+        cout << "Invalid address" << endl;
         return -1;
     }
-    if(connect(sock,(struct sockaddr*)&serv_address,sizeof(serv_address) )<0){
-        cout << "conn failed" <<endl;
+
+    if (connect(sock, (struct sockaddr *)&serv_address, sizeof(serv_address)) < 0) {
+        cout << "Connection Failed" << endl;
         return -1;
     }
-    send(sock,hello,strlen(hello),0);
-    cout << "Hello sent!" << endl;
-    read(sock, buffer, 1024);
+
+    send(sock, hello, strlen(hello), 0);
+    cout << "Hello message sent" << endl;
+    valread = read(sock, buffer, 1024);
     cout << "Message Received: " << buffer << endl;
+
     return 0;
-    }
+}
